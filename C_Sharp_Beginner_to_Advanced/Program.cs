54fb06fbb7448b0017c48a1239917d170a5cf0e0
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-12-21 04:19:00 +0100

    Initial Commit

---
+++ C_Sharp_Beginner_to_Advanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Beginner_to_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
